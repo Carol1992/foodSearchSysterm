ed3ee4d5e9acbff95a2aeac5c60f2bd485af5052
Carol1992 <1689597938@qq.com>
2017-07-09 09:38:39 +0800

    c

--- source/foodName.py
+++ source/foodName.py
@@ -2,6 +2,7 @@ import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from flask_restful import Resource, Api
+from flask_restful import reqparse
 import webbrowser
 
 app = Flask(__name__)
@@ -28,32 +29,37 @@ with app.app_context():
         cur.close()
         return (rv[0] if rv else None) if one else rv
 
-    mysql = 'select code, url, product_name from Food where product_name = ?'
-    food = query_db(mysql, ["Rotini"])
-    if food is None:
-        print ('No such food')
-    else:
+    def getFood(food_idt):
+        print(food_idt.lower())
+        mysql = 'select code, url, product_name from Food where lower(product_name) = ? ' \
+                'or code = ?'
+        food = query_db(mysql, [food_idt.lower(), food_idt.lower()])
         count = 0
         info = []
-        for r in food:
-            count += 1
-            mydict = {
-                'name': r[2],
-                'code': r[0],
-                'url': r[1]
-            }
-            info.append(mydict)
+        if food is None:
+            print ('No such food')
+        else:
+            for r in food:
+                count += 1
+                mydict = {
+                    'name': r[2],
+                    'code': r[0],
+                    'url': r[1]
+                }
+                info.append(mydict)
         myobj = {
             'rcode': '0000',
             'totalCount': count,
             'item': info
         }
+        return myobj
 
-        class getFoodInfo(Resource):
-            def get(self):
-                return myobj
-        api.add_resource(getFoodInfo, '/food')
-        webbrowser.open('http://localhost:5000/food')
+    class getFoodInfo(Resource):
+        def get(self, food_idt):
+            return getFood(food_idt)
+
+    api.add_resource(getFoodInfo, '/name/<string:food_idt>', endpoint='name_ep')
+    #webbrowser.open('http://localhost:5000/food')
 
     if __name__ == "__main__":
         app.run(host= '0.0.0.0')
